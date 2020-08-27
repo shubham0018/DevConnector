4462fe3d2a934b6001e153fc47bdc642250be2df
Shubham <shubham.s0018@gmail.com>
2020-08-27 22:16:15 +0530

    prepare for deployment

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-const connectDb = require("./config/db");
 const connectDB = require("./config/db");
-
+const path = require("path");
 // Connect Database
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Our MERN server has started!")
-})
+  res.send("Our MERN server has started!");
+});
 
 // Init MiddleWare
 app.use(express.json({ extended: false }));
@@ -19,6 +18,17 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server has started on ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Server has started on ${PORT}`);
+});
